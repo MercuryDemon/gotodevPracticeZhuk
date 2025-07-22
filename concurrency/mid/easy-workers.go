3ae73dd6bbfdede8a11c67065b116c0f326f8504
MercuryDemon <nikita190400@gmail.com>
2025-07-22 13:29:20 +0300

    Wrote easy-workers program

--- concurrency/mid/easy-workers.go
+++ concurrency/mid/easy-workers.go
@@ -2,3 +2,67 @@
 // - получают задачи (в задачах спим и что-то печатаем, например) из общего канала.
 // - вычисляют квадрат числа и отправляют результат в общий канал.
 // Главная горутина создаёт N задач, распределяет их по воркерам и выводит результаты.
+package main
+
+import (
+	"log"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+// создаю воркера который будет получать таски из общего для всех воркеров канала tasks и отправлятб в общий канал results
+
+func worker(wg *sync.WaitGroup, inputChan <-chan int, output chan<- int, idx int) {
+
+	log.Printf("[worker] Worker #%v started\n", idx)
+	time.Sleep(200 * time.Millisecond)
+
+	for num := range inputChan {
+		log.Printf("worker #%v calculate square of num %v\n", idx, num) // воркер печатает из общего канала число которое он будет возводить в степень 
+		output <- num * num
+	}
+	wg.Done()
+
+}
+
+func main() {
+
+	log.Println("[main] main() started")
+	var wg sync.WaitGroup          // создаю вейтгруппу чтобы это все не посыпалось
+	var tasks = make(chan int, 10)
+	var results = make(chan int, 10) // создаю каналы
+
+
+  // запускаю три воркера
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go worker(&wg, tasks, results, i+1)
+	}
+
+  // определяю энное число задач 
+	N := rand.Intn(10)
+  
+  // Закидываю таски в канал 
+	for i := 1; i <= N; i++ {
+		if i%2 != 0 {
+			tasks <- i * 2
+		} else {
+			tasks <- (i * 3) - 3
+		}
+	}
+	log.Printf("[main] Wrote %v tasks\n", N)
+
+  //после отправки в канал всех задач, закрываю канал
+	close(tasks)
+
+  //ждём пока все воркеры доработают
+	wg.Wait()
+
+	for i := 1; i <= N; i++ {
+		result := <-results
+		log.Println("[main] Result", i, ":", result) // выводим результаты 
+	}
+	log.Println("[main] main() stopped") 
+
+}
